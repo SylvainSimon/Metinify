41b0fbd619c7bd8c5fa2996da1376b9b513b5d54
SylvanusMan <sylvain.simon@live.fr>
2015-10-31 16:19:53 +0100

    lol

---
+++ js/fonctions.js
@@ -0,0 +1,70 @@
+function createTooltip() {
+    $("[data-tooltip]").each(function () {
+        $(this).attr("title", "");
+
+        var showEffect = "none";
+        var showDelay = 10;
+        var hideEffect = "none";
+        var hideDelay = 10;
+        var persistence = 5000;
+
+        if ($(this).attr("data-tooltip-showEffect") !== undefined) {
+            showEffect = $(this).attr("data-tooltip-showEffect");
+        }
+
+        if ($(this).attr("data-tooltip-showDelay") !== undefined) {
+            showDelay = $(this).attr("data-tooltip-showDelay");
+        }
+
+        if ($(this).attr("data-tooltip-hideEffect") !== undefined) {
+            hideEffect = $(this).attr("data-tooltip-hideEffect");
+        }
+
+        if ($(this).attr("data-tooltip-hideDelay") !== undefined) {
+            hideDelay = $(this).attr("data-tooltip-hideDelay");
+        }
+
+        if ($(this).attr("data-tooltip-persistence") !== undefined) {
+            persistence = $(this).attr("data-tooltip-persistence");
+        }
+
+        var positionTooltip = {my: 'center top', at: 'center bottom+10'};
+        var tooltipClass = "bottom";
+
+        if ($(this).attr("data-tooltip-position") !== undefined) {
+
+            switch ($(this).attr("data-tooltip-position")) {
+                case "left":
+                    positionTooltip = {my: 'right center', at: 'left-10 center'};
+                    tooltipClass = "left";
+
+                    break;
+            }
+
+        }
+        positionTooltip.collision = 'none';
+
+        $(this).tooltip({
+            position: positionTooltip,
+            tooltipClass: tooltipClass,
+            show: {effect: showEffect, delay: showDelay},
+            hide: {effect: hideEffect, delay: hideDelay},
+            content: function () {
+                return $(this).attr('data-tooltip');
+            },
+            open: function (event, ui) {
+                setTimeout(function () {
+                    $(ui.tooltip).hide('blind');
+                }, persistence);
+            }
+        });
+    });
+}
+
+function redraw() {
+    createTooltip();
+}
+
+$(document).ready(function () {
+    redraw();
+});
