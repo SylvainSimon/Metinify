54db71554605455f651243732b5dc2da8f69ae8d
SylvanusMan <sylvain.simon@live.fr>
2015-11-01 11:10:58 +0100

    changement e-mail

--- pages/Compte_Changer_Mot_De_Passe_Mail_Envoye.php
+++ pages/Compte_Changer_Mot_De_Passe_Mail_Envoye.php
@@ -1,57 +1,62 @@
 <?php @session_write_close(); ?>
 <?php @session_start(); ?>
 
-<div class="Cadre_Principal">
+<div class="box box-default flat">
 
-    <div class="Cadre_Principal_Haut Pointer No_Select" onclick="Slider_Cadre_Principal_1();">                  
-        <h1>Envoie du mail</h1>
+    <div class="box-header">
+        <h3 class="box-title">Envoie du mail</h3>
     </div>
-    <div class="Cadre_Principal_Milieu" id="Div_Cadre_Principal_1">
-        <hr class="Hr_Haut"/>
-        Un mail vien de vous être envoyé, merci de noter, ci-après, le code unique.<br/>
 
-        <form action="javascript:void(0)" id="Formulaire_Verif_Changer_Mail" name="FormInscription" method="POST">
+    <form action="javascript:void(0)" id="Formulaire_Verif_Changer_Mail" name="FormInscription" method="POST">
 
-            <table class="Table_Modele">
-                <tr> 
-                    <td class="Colonne_Gauche_Formulaire">
-                        Code confidentielle :
-                    </td>  
-                    <td>
-                        <input type="text" class="Zone_Saisie_Inscription" id="Code_Confidentiel" placeholder="Code confidentiel" name="Code_Confidentiel">
-                    </td>
-                    <td class="Colonne_Droite_Formulaire"> 
-                        Indiquez le code reçu par E-mail.
-                    </td>
-                </tr>
-            </table>
-            <hr class="Hr_Bas">
-            <input class="Bouton_Formulaire_Inscription Bouton_Normal" style="position: relative; left:285px !important;" type="button" onclick="Verification_Code_Confidentiel();" value="Valider" />
+        <div class="box-body">
 
-        </form>
+            Un mail vien de vous être envoyé, merci de noter, ci-après, le code unique.<br/>
 
-    </div>
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="form-group ">
+                        <label for="Code_Confidentiel">
+                            Code confidentielle
+                        </label>
+
+                        <div class="input-group col-xs-12">
+                            
+                        <input type="text" class="form-control input-sm text" id="Code_Confidentiel" placeholder="Code confidentiel" name="Code_Confidentiel">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="box-footer">
+            <input class="btn btn-success btn-flat" type="button" onclick="Verification_Code_Confidentiel();" value="Valider" />
+        </div>
+
+    </form>
 </div>
 
 <script type="text/javascript">
 
-    function Verification_Code_Confidentiel(){
-        
+    function Verification_Code_Confidentiel() {
+
         Barre_De_Statut("Vérification du code...");
         Icone_Chargement(1);
-        
+
         $.ajax({
             type: "POST",
             url: "ajax/SQL_Changer_Mot_De_Passe_Verif_Code.php",
-            data: "code="+$("#Code_Confidentiel").val(),
-            success: function(msg){
-                
-                if(msg==1){
-                    
+            data: "code=" + $("#Code_Confidentiel").val(),
+            success: function (msg) {
+
+                if (msg == 1) {
+
                     Ajax("pages/Compte_Changer_Mot_De_Passe_Confirmation.php");
                 }
-                else{
-                    
+                else {
+
                     Barre_De_Statut("Le code de vérification est incorrect.");
                     Icone_Chargement(2);
                 }
