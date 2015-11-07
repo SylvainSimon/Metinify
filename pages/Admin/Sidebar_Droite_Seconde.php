6b4620ac53180511c89cfa48ad6dffc79fed364b
SylvanusMan <sylvain.simon@live.fr>
2015-11-07 19:14:38 +0100

    ghcghfd

--- pages/Admin/Sidebar_Droite_Seconde.php
+++ pages/Admin/Sidebar_Droite_Seconde.php
@@ -7,10 +7,10 @@
 
         <table class="table table-condensed table-hover" style="border-collapse: collapse;">
             <?php if ($this->arrAdminRights["gererMonnaies"]) { ?>
-                <tr class="fancybox_monnaies" onclick="Ajax('pages/Admin/Gestion_Monnaies.php');"><td> Gestion des monnaies</td></tr>
+                <tr onclick="Ajax('pages/Admin/Gestion_Monnaies.php');"><td> Gestion des monnaies</td></tr>
             <?php } ?>
             <?php if ($this->arrAdminRights["gererNews"]) { ?>
-                <tr onclick="Ajax('pages/Admin/includes/Gestion_News.php')"><td> Gestion des news</td></tr>
+                <tr onclick="Ajax('pages/Admin/Gestion_News.php')"><td> Gestion des news</td></tr>
             <?php } ?>
         </table>
     </div>
