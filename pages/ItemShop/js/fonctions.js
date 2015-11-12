851db7e277d7014783422a17f7c7dbec8ce1d02d
SylvanusMan <sylvain.simon@live.fr>
2015-11-12 20:31:34 +0100

    fghjfghjfghj

--- pages/ItemShop/js/fonctions.js
+++ pages/ItemShop/js/fonctions.js
@@ -1,3 +1,28 @@
+function Appel_Categorie_ItemShop(id) {
+
+    Barre_De_Statut("Chargement de la catégorie...");
+    Icone_Chargement(1);
+    displayLoading();
+
+    $.ajax({
+        type: "POST",
+        url: "pages/ItemShop/ajax/ajaxCategorieGetArticles.php",
+        data: "id=" + id,
+        success: function (msg) {
+
+            $("#Tableau_Liste_Article").html(msg);
+            Barre_De_Statut("Catégorie chargé.");
+            Icone_Chargement(0);
+
+            hideLoading();
+            redraw();
+
+        }
+    });
+    return false;
+
+}
+
 function Valider_Mon_Achat(id_item, nombre_item) {
 
     bootbox.dialog({
@@ -20,7 +45,7 @@ function Valider_Mon_Achat(id_item, nombre_item) {
 
                     Barre_De_Statut("Transaction en cours...");
                     Icone_Chargement(1);
-
+                    displayLoading();
 
                     $.ajax({
                         type: "POST",
@@ -32,6 +57,7 @@ function Valider_Mon_Achat(id_item, nombre_item) {
 
                             if (data.result === 1) {
 
+                                hideLoading();
                                 Ajax("pages/ItemShop/ItemShopAchatTerm.php?idTransaction=" + data.idTransaction);
 
                             } else {
