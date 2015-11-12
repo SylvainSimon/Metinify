070dc2598f5f71392053e5ea837386c02dc52596
SylvanusMan <sylvain.simon@live.fr>
2015-11-12 23:05:39 +0100

    fghj

--- pages/_Home/includes/Menu_Primaire.php
+++ pages/_Home/includes/Menu_Primaire.php
@@ -1,15 +1,16 @@
 <nav id="mainnav">
     <ul class="sidebar-menu">
-        <li><a href="javascript:void(0)" onclick="Ajax('pages/_LegacyPages/News.php')">Accueil</a></li>
-        <li><a href="javascript:void(0)" onclick="Ajax('pages/_LegacyPages/Presentation.php')">Présentation</a></li>
+        
+        <li class="pointer"><a onclick="Ajax('pages/_LegacyPages/News.php')">Accueil</a></li>
+        <li class="pointer"><a onclick="Ajax('pages/_LegacyPages/Presentation.php')">Présentation</a></li>
 
 
         <?php if (empty($_SESSION['Utilisateur'])) { ?>
-            <li id="Menu_Inscription_MonCompte2"><a href="javascript:void(0)" onclick="Ajax('pages/Inscription/InscriptionForm.php')">Inscription</a></li>
-            <li id="Menu_Inscription_MonCompte" style="display: none;"><a id="Lien_Mon_Compte" href="javascript:void(0)" onclick="">Mon compte</a></li>
+            <li id="Menu_Inscription_MonCompte2" class="pointer"><a onclick="Ajax('pages/Inscription/InscriptionForm.php')">Inscription</a></li>
+            <li id="Menu_Inscription_MonCompte" class="pointer" style="display: none;"><a id="Lien_Mon_Compte" onclick="">Mon compte</a></li>
         <?php } else { ?>
-            <li id="Menu_Inscription_MonCompte"><a id="Lien_Mon_Compte" href="javascript:void(0)" onclick="Ajax('pages/MonCompte/modules/MonCompte.php?id=<?php echo $_SESSION['ID']; ?>')">Mon compte</a></li>
-            <li id="Menu_Inscription_MonCompte2" style="display: none;"><a href="javascript:void(0)" onclick="Ajax('pages/Inscription/InscriptionForm.php')">Inscription</a></li>
+            <li id="Menu_Inscription_MonCompte" class="pointer"><a id="Lien_Mon_Compte" onclick="Ajax('pages/MonCompte/modules/MonCompte.php?id=<?php echo $_SESSION['ID']; ?>')">Mon compte</a></li>
+            <li id="Menu_Inscription_MonCompte2" class="pointer" style="display: none;"><a onclick="Ajax('pages/Inscription/InscriptionForm.php')">Inscription</a></li>
         <?php } ?>
 
         <li class="treeview">
@@ -19,136 +20,42 @@
             </a>
             <ul class="treeview-menu">
                 <li>
-                    <a href="#" onclick="Ajax('pages/Classements/ClassementJoueursPvE.php')">Joueurs PVE</a>
-                    <a href="#" onclick="Ajax('pages/Classements/ClassementJoueursPvP.php')">Joueurs PVP</a>
-                    <a href="#" onclick="Ajax('pages/Classements/ClassementGuildes.php')">Guildes</a>
+                    <a class="pointer" onclick="Ajax('pages/Classements/ClassementJoueursPvE.php')">Joueurs PVE</a>
+                    <a class="pointer" onclick="Ajax('pages/Classements/ClassementJoueursPvP.php')">Joueurs PVP</a>
+                    <a class="pointer" onclick="Ajax('pages/Classements/ClassementGuildes.php')">Guildes</a>
                 </li>
             </ul>
         </li>
-        
+
         <?php if (empty($_SESSION['Utilisateur'])) { ?>
-            <li id="Menu_Telechargement_ItemShop2" style="display: inline;"><a href="javascript:void(0)" onclick="Ajax('pages/_LegacyPages/Telechargement.php')">Téléchargement</a></li>
-            <li id="Menu_Telechargement_ItemShop" style="display: none;"><a id="Lien_Item_Shop" href="javascript:void(0)" onclick="" >Item-Shop</a></li>
+            <li id="Menu_Telechargement_ItemShop2" class="pointer" style="display: inline;"><a onclick="Ajax('pages/_LegacyPages/Telechargement.php')">Téléchargement</a></li>
+            <li id="Menu_Telechargement_ItemShop" class="pointer" style="display: none;"><a id="Lien_Item_Shop" onclick="" >Item-Shop</a></li>
         <?php } else { ?>
-            <li id="Menu_Telechargement_ItemShop" style="display: inline;"><a id="Lien_Item_Shop" href="javascript:void(0)" onclick="Ajax('pages/ItemShop/ItemShop.php?id=<?php echo $_SESSION['ID']; ?>')">Item-Shop</a></li>
-            <li id="Menu_Telechargement_ItemShop2"><a href="javascript:void(0)" onclick="Ajax('pages/_LegacyPages/Telechargement.php')">Téléchargement</a></li>
+            <li id="Menu_Telechargement_ItemShop" class="pointer" style="display: inline;"><a id="Lien_Item_Shop" onclick="Ajax('pages/ItemShop/ItemShop.php?id=<?php echo $_SESSION['ID']; ?>')">Item-Shop</a></li>
+            <li id="Menu_Telechargement_ItemShop2" class="pointer"><a onclick="Ajax('pages/_LegacyPages/Telechargement.php')">Téléchargement</a></li>
         <?php } ?>
 
         <?php if (empty($_SESSION['Utilisateur'])) { ?>
-            <li id="Menu_Telechargement_Equipe2" style="display: inline;"><a href="javascript:void(0)" onclick="Ajax('pages/_LegacyPages/Calendrier.php')">Calendrier des events</a></li>
-            <li id="Menu_Telechargement_Equipe" style="display: none;"><a id="Lien_Marche" href="javascript:void(0)" onclick="" >Marché des personnages</a></li>
+            <li id="Menu_Telechargement_Equipe2" class="pointer" style="display: inline;"><a onclick="Ajax('pages/_LegacyPages/Calendrier.php')">Calendrier des events</a></li>
+            <li id="Menu_Telechargement_Equipe" class="pointer" style="display: none;"><a id="Lien_Marche" onclick="" >Marché des personnages</a></li>
         <?php } else { ?>
-            <li id="Menu_Telechargement_Equipe" style="display: inline;"><a id="Lien_Marche" href="javascript:void(0)" onclick="Ajax('pages/Marche/Marche.php')">Marché des personnages</a></li>
-            <li id="Menu_Telechargement_Equipe2" style="display: none;"><a href="javascript:void(0)" onclick="Ajax('pages/_LegacyPages/Calendrier.php')">Calendrier</a></li>
+            <li id="Menu_Telechargement_Equipe" class="pointer" style="display: inline;"><a id="Lien_Marche" onclick="Ajax('pages/Marche/Marche.php')">Marché des personnages</a></li>
+            <li id="Menu_Telechargement_Equipe2" class="pointer" style="display: none;"><a onclick="Ajax('pages/_LegacyPages/Calendrier.php')">Calendrier</a></li>
         <?php } ?>
 
-        <li><a href="javascript:void(0)" onclick="window.open('http://forum.vamosmt2.org/forum/')">Notre forum</a></li>
+        <li><a onclick="window.open('http://forum.vamosmt2.org/forum/')">Notre forum</a></li>
 
         <?php if (empty($_SESSION['Utilisateur'])) { ?>
-            <li id="Menu_Support2"><a href="javascript:void(0)" onclick="Ajax('pages/_LegacyPages/Contacts.php')">Support</a></li>
-            <li id="Menu_Support" style="display: none;"><a id="Lien_Support" href="javascript:void(0)" onclick="">Support</a></li>
+            <li id="Menu_Support2" class="pointer"><a onclick="Ajax('pages/_LegacyPages/Contacts.php')">Support</a></li>
+            <li id="Menu_Support" class="pointer" style="display: none;"><a id="Lien_Support" onclick="">Support</a></li>
         <?php } else { ?>
-            <li id="Menu_Support"><a href="javascript:void(0)" id="Lien_Support" onclick="Ajax('pages/Messagerie/Messagerie.php')">Support</a></li>
-            <li id="Menu_Support2" style="display: none;"><a href="javascript:void(0)" onclick="Ajax('pages/_LegacyPages/Contacts.php');">Support</a></li>
+            <li id="Menu_Support" class="pointer"><a id="Lien_Support" onclick="Ajax('pages/Messagerie/Messagerie.php')">Support</a></li>
+            <li id="Menu_Support2" class="pointer" style="display: none;"><a onclick="Ajax('pages/_LegacyPages/Contacts.php');">Support</a></li>
             <?php } ?>
     </ul>
 </nav>
 
 <script type="text/javascript">
-    $(document).ready(function () {
-        $(".fancybox_Mon_Compte").fancybox({
-            minWidth: 1200,
-            minHeight: 550,
-            maxHeight: 550,
-            padding: 0,
-            closeBtn: false,
-            scrolling: 'no',
-            scrollOutside: false,
-            fitToView: true,
-            autoSize: false,
-            closeClick: false,
-            openEffect: 'elastic',
-            closeEffect: 'elastic',
-            openSpeed: 400,
-            closeSpeed: 200
-        });
-    });
-
-    $(document).ready(function () {
-        $(".fancybox_Marche").fancybox({
-            minWidth: 1200,
-            minHeight: 550,
-            maxHeight: 550,
-            padding: 0,
-            closeBtn: false,
-            scrolling: 'no',
-            scrollOutside: false,
-            fitToView: true,
-            autoSize: false,
-            closeClick: false,
-            openEffect: 'elastic',
-            closeEffect: 'elastic',
-            openSpeed: 400,
-            closeSpeed: 200
-        });
-    });
-
-    $(document).ready(function () {
-        $(".fancybox_Vote").fancybox({
-            minWidth: 1200,
-            minHeight: 550,
-            maxHeight: 550,
-            padding: 0,
-            closeBtn: false,
-            scrolling: 'no',
-            scrollOutside: false,
-            fitToView: true,
-            autoSize: false,
-            closeClick: false,
-            openEffect: 'elastic',
-            closeEffect: 'elastic',
-            openSpeed: 400,
-            closeSpeed: 200
-        });
-    });
-
-    $(document).ready(function () {
-        $(".fancybox_Messagerie").fancybox({
-            minWidth: 1000,
-            minHeight: 550,
-            maxHeight: 550,
-            padding: 0,
-            closeBtn: false,
-            scrolling: 'no',
-            scrollOutside: false,
-            fitToView: true,
-            autoSize: false,
-            closeClick: false,
-            openEffect: 'elastic',
-            closeEffect: 'elastic',
-            openSpeed: 400,
-            closeSpeed: 200
-        });
-    });
-
-    $(document).ready(function () {
-        $(".fancybox_ItemShop").fancybox({
-            minWidth: 780,
-            minHeight: 550,
-            maxHeight: 550,
-            padding: 0,
-            closeBtn: false,
-            scrolling: 'no',
-            scrollOutside: false,
-            fitToView: true,
-            autoSize: false,
-            closeClick: false,
-            openEffect: 'elastic',
-            closeEffect: 'elastic',
-            openSpeed: 400,
-            closeSpeed: 200
-        });
-    });
 
     $(document).ready(function () {
         $(".fancybox_Rechargement").fancybox({
@@ -162,25 +69,6 @@
             height: 450,
             closeClick: false,
             topRatio: 0.5,
-            openEffect: 'elastic',
-            closeEffect: 'elastic',
-            openSpeed: 400,
-            closeSpeed: 200
-        });
-    });
-
-    $(document).ready(function () {
-        $(".fancybox_Statistiques").fancybox({
-            minWidth: 1000,
-            minHeight: 550,
-            maxHeight: 550,
-            padding: 0,
-            closeBtn: false,
-            scrolling: 'no',
-            scrollOutside: false,
-            fitToView: true,
-            autoSize: false,
-            closeClick: false,
             openEffect: 'elastic',
             closeEffect: 'elastic',
             openSpeed: 400,
