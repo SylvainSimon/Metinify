47d1b0273b9076825c76063d9c8f7f583d84d5fb
SylvanusMan <sylvain.simon@live.fr>
2015-11-16 23:39:22 +0100

    dfghdfgh

--- pages/Messagerie/Messagerie.php
+++ pages/Messagerie/Messagerie.php
@@ -12,12 +12,19 @@ class Messagerie extends \PageHelper {
 
     public function run() {
 
+        $templateTop = $this->objTwig->loadTemplate("MessagerieInbox.html5.twig");
+        $arrObjSupportDiscussions = \Site\SiteHelper::getSupportDiscussionsRepository()->findDiscussions($this->objAccount->getId(), false, 20);
+        $countMessageNonLu = \Site\SiteHelper::getSupportMessagesRepository()->countMessagesNonLu($this->objAccount->getId());
+        $viewInbox = $templateTop->render(["arrObjSupportDiscussions" => $arrObjSupportDiscussions, "countMessageNonLu" => $countMessageNonLu]);
+        
         $countModerateur = \Site\SiteHelper::getSupportModerateursRepository()->countByIdAccount($this->objAccount->getId());
         if ($countModerateur > 0) {
             $this->arrayTemplate["isModerateurTicket"] = true;
         } else {
             $this->arrayTemplate["isModerateurTicket"] = false;
         }
+        
+        $this->arrayTemplate["viewInbox"] = $viewInbox;
         
         $view = $this->template->render($this->arrayTemplate);
         $this->response->setContent($view);
