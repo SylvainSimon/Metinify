b25e994418d2de5ce0452a3b6dc1e69a36964df2
SylvanusMan <sylvain.simon@live.fr>
2015-12-09 14:05:37 +0100

    fghjfghjfghj

--- pages/Admin/modules/GererItemShop/ajax/ajaxGererItemShopCategoriesDelete.php
+++ pages/Admin/modules/GererItemShop/ajax/ajaxGererItemShopCategoriesDelete.php
@@ -1,16 +1,53 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Administration;
 
-/**
- * Description of ajaxGererItemShopCategoriesDelete
- *
- * @author Sylvanus
- */
-class ajaxGererItemShopCategoriesDelete {
-    //put your code here
+require __DIR__ . '../../../../../../core/initialize.php';
+
+class ajaxGererItemShopArticlesDelete extends \PageHelper {
+
+    public $isProtected = true;
+    public $isAdminProtected = true;
+
+    public function __construct() {
+        parent::__construct();
+        $this->VerifyTheRight(\DroitsHelper::GERER_ITEMSHOP_CATEGORIE);
+    }
+
+    public function run() {
+
+        global $request;
+        $em = \Shared\DoctrineHelper::getEntityManager();
+
+        $idCategorie = $request->request->get("idCategorie");
+
+        if ($idCategorie > 0) {
+
+            $objItemshopCategories = \Site\SiteHelper::getItemshopCategoriesRepository()->findByCat($idCategorie);
+            
+            if ($objItemshopCategories !== null) {
+                $em->remove($objItemshopCategories);
+                $em->flush();
+                $result = array(
+                    'result' => true,
+                    'reasons' => ""
+                );
+            } else {
+                $result = array(
+                    'result' => false,
+                    'reasons' => "La catégorie sélectionné n'éxiste pas."
+                );
+            }
+        } else {
+            $result = array(
+                'result' => false,
+                'reasons' => "Problème de transmission d'identifiant."
+            );
+        }
+        echo json_encode($result);
+    }
+
 }
+
+$class = new ajaxGererItemShopArticlesDelete();
+$class->run();
